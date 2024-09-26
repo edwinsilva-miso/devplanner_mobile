5f2df4bc9eb92633ff36e66cf33732cf6a3f1ff4
Edwin Silva <ea.silval1@uniandes.edu.co>
2024-09-26 18:50:16 -0500

    Add activity navigation from main to login

--- app/src/main/java/com/example/devplannermobile/MainActivity.kt
+++ app/src/main/java/com/example/devplannermobile/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.devplannermobile
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val b1 = findViewById<Button>(R.id.btnMail)
+        b1.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
